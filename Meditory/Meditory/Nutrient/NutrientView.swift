ea498483889cac5b0ee6ece44f218d1958fb8520
김재훈 <jaehun6165@naver.com>
2025-08-01 10:35:32 +0900

    SCRUM-36/ 뷰 생성

---
+++ Meditory/Meditory/Nutrient/NutrientView.swift
@@ -0,0 +1,18 @@
+//
+//  NutrientView.swift
+//  Meditory
+//
+//  Created by Jaehun Kim on 8/1/25.
+//
+
+import SwiftUI
+
+struct NutrientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NutrientView()
+}
