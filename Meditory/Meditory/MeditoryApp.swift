f55ab41621b52da49a4c49a0626bec937a70de3e
윤혜주 <assaw102@gmail.com>
2025-08-01 09:18:35 +0900

    chore:Firebase 추가

--- Meditory/Meditory/MeditoryApp.swift
+++ Meditory/Meditory/MeditoryApp.swift
@@ -7,9 +7,13 @@
 
 import SwiftUI
 import SwiftData
+import FirebaseCore
 
 @main
 struct MeditoryApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
