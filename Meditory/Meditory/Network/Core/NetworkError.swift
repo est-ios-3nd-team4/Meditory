65af7c8fbd7753775586ecedfabb5327a3e4b2aa
윤혜주 <assaw102@gmail.com>
2025-08-01 16:18:37 +0900

    작업 중인 변경사항 저장

---
+++ Meditory/Meditory/Network/Core/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  Meditory
+//
+//  Created by 홍승아 on 8/1/25.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidRequest
+    case unprocessableEntity
+}
