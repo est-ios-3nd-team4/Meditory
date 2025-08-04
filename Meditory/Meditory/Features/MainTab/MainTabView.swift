9b56ee0961a19108839fef5594616e0b768093c3
8zipcore <howtofindjayu@gmail.com>
2025-08-04 19:30:07 +0900

    feat: MainTabView 구현 및 첫화면으로 설정 (SCRUM-52)

---
+++ Meditory/Meditory/Features/MainTab/MainTabView.swift
@@ -0,0 +1,52 @@
+//
+//  MainTabView.swift
+//  Meditory
+//
+//  Created by 홍승아 on 8/4/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+  
+  @State var selectedTab: TabItem = .home
+  
+  var body: some View {
+    if UIDevice.isPad {
+      
+    } else {
+      GeometryReader { geometry in
+        let tabViewHeight = geometry.size.width * 0.28
+        
+        ZStack {
+          Group {
+            switch selectedTab {
+            case .home:
+              HomeView()
+            case .recommend:
+              RecommendView()
+            case .add:
+              Color.red
+            case .dailyNutrition:
+              Color.green
+            case .settings:
+              Color.white
+            }
+          }
+          
+          VStack {
+            Spacer()
+            
+            CustomTabView(selectedTab: $selectedTab)
+              .frame(height: tabViewHeight)
+          }
+        }
+      }
+      .ignoresSafeArea(edges: .bottom)
+    }
+  }
+}
+
+#Preview {
+  MainTabView()
+}
