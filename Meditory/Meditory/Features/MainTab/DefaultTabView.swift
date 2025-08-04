afb82ba380cb280e72fefa7866ba80f5e0e605d5
8zipcore <howtofindjayu@gmail.com>
2025-08-04 19:29:42 +0900

    feat: DefaultTabView 구현 (SCRUM-52)

---
+++ Meditory/Meditory/Features/MainTab/DefaultTabView.swift
@@ -0,0 +1,39 @@
+//
+//  DefaultTabView.swift
+//  Meditory
+//
+//  Created by 홍승아 on 8/4/25.
+//
+
+import SwiftUI
+
+struct DefaultTabView: View {
+  
+  var body: some View {
+    TabView() {
+      HomeView()
+        .tabItem {
+          Text(TabItem.home.title)
+        }
+      
+      RecommendView()
+        .tabItem {
+          Text(TabItem.recommend.title)
+        }
+      
+      Color.green
+        .tabItem {
+          Text(TabItem.dailyNutrition.title)
+        }
+      
+      Color.white
+        .tabItem {
+          Text(TabItem.settings.title)
+        }
+    }
+  }
+}
+
+#Preview {
+  DefaultTabView()
+}
