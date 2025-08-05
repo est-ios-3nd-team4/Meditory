46c824f94f30d75a740597157fb9849986df19ec
김현식 <dawnstory.white@gmail.com>
2025-08-05 13:24:40 +0900

    feat:SCRUM-38 온보딩화면 리스트뷰 별도 구현

---
+++ Meditory/Meditory/Features/Onboarding/OnboardingListSelectionView.swift
@@ -0,0 +1,38 @@
+//
+//  OnboardingListSelectionView.swift
+//  Meditory
+//
+//  Created by hyunsic on 8/5/25.
+//
+
+import SwiftUI
+
+struct OnboardingListSelectionView: View {
+  let prompt:String
+  let info:String
+  var questions:[String]
+  @Binding var selections:Set<String>
+  var onAction:((String)->Void)?
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .leading){
+        Text(prompt)
+          .font(.custom("NotoSansKR-Bold", size: 24))
+          .padding(.vertical, 10)
+        Text(info)
+          .font(.custom("NotoSansKR-Medium", size: 12))
+          .foregroundStyle(.gray)
+      }
+      ForEach(questions,id:\.self) { item in
+        RowItemView(isSelected: selections.contains(item), context: item)
+          .onTapGesture {
+            onAction?(item)
+          }
+      }
+    }
+  }
+}
+
+#Preview {
+  OnboardingListSelectionView(prompt: "앓고 계신 질환을 선택해주세요", info: "알레르기", questions: ["a","b","c"], selections: .constant(["Set<String>"]))
+}
