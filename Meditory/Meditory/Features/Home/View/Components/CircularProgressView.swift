96cf27d4719964479782e496f54fdeb49dd8521d
윤혜주 <assaw102@gmail.com>
2025-08-05 16:05:30 +0900

    feat: 차트 그래프 애니메이션 개선

--- Meditory/Meditory/Features/Home/View/Components/CircularProgressView.swift
+++ Meditory/Meditory/Features/Home/View/Components/CircularProgressView.swift
@@ -15,61 +15,58 @@ struct CircularProgressView: View {
     var body: some View {
         GeometryReader { geo in
             let side = min(geo.size.width, geo.size.height)
-            let center = CGPoint(x: geo.size.width/2, y: geo.size.height/2)
-
-            let strokeRadius = side / 2
-            let angle = Angle(degrees: progress * 360 - 90)
-            let radians = CGFloat(angle.radians)
-
-            let knobCenter = CGPoint(
-                x: center.x + cos(radians) * strokeRadius,
-                y: center.y + sin(radians) * strokeRadius
-            )
+            let center = CGPoint(x: side/2, y: side/2)
+            let radius = (side - lineWidth) / 2
 
             ZStack {
                 // 그래프 배경 회색
-                Circle().stroke(Color.chartBackground, lineWidth: lineWidth)
+                Circle()
+                    .inset(by: lineWidth/2)
+                    .stroke(Color.chartBackground, lineWidth: lineWidth)
 
-                Group {
-                    // 그래프
-                    Circle()
-                        .trim(from: 0, to: progress)
-                        .stroke(
-                            AngularGradient(
-                                gradient: Gradient(colors: [Color.sub, Color.main, Color.sub ]),
-                                center: .center
-                            ),
-                            style: StrokeStyle(lineWidth: lineWidth, lineCap: .round)
-                        )
-                        .rotationEffect(.degrees(-90))
+                // 그래프
+                Circle()
+                    .inset(by: lineWidth/2)
+                    .trim(from: 0, to: progress)
+                    .stroke(
+                        AngularGradient(
+                            gradient: Gradient(colors: [Color.sub, Color.main, Color.sub]),
+                            center: .center
+                        ),
+                        style: StrokeStyle(lineWidth: lineWidth, lineCap: .round)
+                    )
+                    .rotationEffect(.degrees(-90))
+                    .animation(.easeInOut(duration: 0.6), value: progress)
 
-                    // 그래프 위 원
-                    Circle()
-                        .fill(Color.white)
-                        .frame(width: lineWidth, height: lineWidth)
-                        .overlay(
-                            Circle()
-                                .fill(Color.main)
-                                .frame(width: lineWidth * 0.5,
-                                       height: lineWidth * 0.5)
-                        )
-                        .position(knobCenter)
-                        .zIndex(2)
-                }
-                .animation(.easeInOut(duration: 0.6), value: progress)
+                // 그래프 위 원
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: lineWidth, height: lineWidth)
+                    .overlay(
+                        Circle()
+                            .fill(Color.main)
+                            .frame(width: lineWidth * 0.5, height: lineWidth * 0.5)
+                    )
+                    .offset(x: 0, y: -radius)
+                    .rotationEffect(.degrees(progress * 360))
+                    .animation(.easeInOut(duration: 0.6), value: progress)
 
                 // 퍼센트
-                HStack(alignment: .firstTextBaseline) {
+                HStack(alignment: .firstTextBaseline, spacing: 0) {
                     Text("\(Int(progress * 100))")
-                        .font(.notoSans(size: 60))
-                        .fontWeight(.bold)
+                        .font(.notoSans(size: 50))
                     Text("%")
-                        .font(.notoSans(size: 20))
+                        .font(.notoSans(size: 15))
+                        .offset(y: -10)
                 }
                 .foregroundColor(.label)
+                .position(center)
             }
             .frame(width: side, height: side)
         }
         .aspectRatio(1, contentMode: .fit)
     }
+}
+#Preview {
+    HomeView()
 }
