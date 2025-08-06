6bdfbd33d13badb194a46131c2ed0cd312db51c9
LEE CHIHOON <dlclgns152615@gmail.com>
2025-08-06 13:32:54 +0900

    feat: dailynutritionview-ui 지금까지 개발한 내용 (SCRUM-60)

--- Meditory/Meditory/Features/DailyNutrition/DailyNutritionView.swift
+++ Meditory/Meditory/Features/DailyNutrition/DailyNutritionView.swift
@@ -7,10 +7,45 @@
 
 import SwiftUI
 
+enum MacroNutrient {
+  case carbohydrate
+  case protein
+  case fat
+}
+
 struct DailyNutritionView: View {
   
   var body: some View {
-    Text("DailyNutritionView")
+    CalendarBackgroundView {
+      VStack {
+        ZStack {
+          Rectangle()
+            .fill(Color.white)
+            .frame(height: 140)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+          
+          HStack {
+            Text("오늘 하루 식단")
+              .foregroundStyle(.black)
+            
+            VStack {
+              HStack {
+                Circle()
+                  .fill(.customCarbohydrate)
+                  .frame(width: 10, height: 10)
+                
+                Text("탄수화물")
+                  .foregroundStyle(.black)
+              }
+            }
+          }
+        }
+      }
+    }
   }
   
+}
+
+#Preview {
+  DailyNutritionView()
 }
