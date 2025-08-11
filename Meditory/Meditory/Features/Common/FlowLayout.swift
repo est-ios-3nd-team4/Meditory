7b667a1b12ef1bf956dcf12bb10f0c2aa00a067a
drfranken <daranic9@gmail.com>
2025-08-11 17:48:11 +0900

    fix: PR리뷰반영_안쓰이는변수제거, 인덴트적용 (SCRUM-121)

--- Meditory/Meditory/Features/Common/FlowLayout.swift
+++ Meditory/Meditory/Features/Common/FlowLayout.swift
@@ -13,54 +13,54 @@
 import SwiftUI
 
 struct FlowLayout: Layout {
-    var spacing: CGFloat = 8
-    var lineSpacing: CGFloat = 8
-
-    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-        var width: CGFloat = 0
-        var height: CGFloat = 0
-        var lineHeight: CGFloat = 0
-        let maxWidth = proposal.width ?? .infinity
-
-        for subview in subviews {
-            let size = subview.sizeThatFits(.unspecified)
-
-            if width + size.width > maxWidth && width > 0 {
-                // 줄바꿈
-                width = 0
-                height += lineHeight + lineSpacing
-                lineHeight = 0
-            }
-
-            width += size.width + spacing
-            lineHeight = max(lineHeight, size.height)
-        }
-
-        // 마지막 줄 반영
-        height += lineHeight
-        return CGSize(width: maxWidth, height: height)
+  var spacing: CGFloat = 8
+  var lineSpacing: CGFloat = 8
+  
+  func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+    var width: CGFloat = 0
+    var height: CGFloat = 0
+    var lineHeight: CGFloat = 0
+    let maxWidth = proposal.width ?? .infinity
+    
+    for subview in subviews {
+      let size = subview.sizeThatFits(.unspecified)
+      
+      if width + size.width > maxWidth && width > 0 {
+        // 줄바꿈
+        width = 0
+        height += lineHeight + lineSpacing
+        lineHeight = 0
+      }
+      
+      width += size.width + spacing
+      lineHeight = max(lineHeight, size.height)
     }
-
-    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        var x: CGFloat = bounds.minX
-        var y: CGFloat = bounds.minY
-        var lineHeight: CGFloat = 0
-        let maxWidth = bounds.width
-
-        for subview in subviews {
-            let size = subview.sizeThatFits(.unspecified)
-
-            // 줄바꿈
-            if x + size.width > bounds.maxX && x > bounds.minX {
-                x = bounds.minX
-                y += lineHeight + lineSpacing
-                lineHeight = 0
-            }
-
-            subview.place(at: CGPoint(x: x, y: y),
-                          proposal: ProposedViewSize(size))
-            x += size.width + spacing
-            lineHeight = max(lineHeight, size.height)
-        }
+    
+    // 마지막 줄 반영
+    height += lineHeight
+    return CGSize(width: maxWidth, height: height)
+  }
+  
+  func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+    var x: CGFloat = bounds.minX
+    var y: CGFloat = bounds.minY
+    var lineHeight: CGFloat = 0
+    let maxWidth = bounds.width // ✅ 이제 실제로 사용됨
+    
+    for subview in subviews {
+      let size = subview.sizeThatFits(.unspecified)
+      
+      // 줄바꿈
+      if x + size.width > maxWidth + bounds.minX && x > bounds.minX {
+        x = bounds.minX
+        y += lineHeight + lineSpacing
+        lineHeight = 0
+      }
+      
+      subview.place(at: CGPoint(x: x, y: y),
+                    proposal: ProposedViewSize(size))
+      x += size.width + spacing
+      lineHeight = max(lineHeight, size.height)
     }
+  }
 }
