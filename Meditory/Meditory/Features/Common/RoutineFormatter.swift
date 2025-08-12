a41bee962c4f341ca469b72e18ec7f700cc22a5e
윤혜주 <assaw102@gmail.com>
2025-08-12 13:27:27 +0900

    fix: 요일 인덱스 변경 (일=0, 토=6)

--- Meditory/Meditory/Features/Common/RoutineFormatter.swift
+++ Meditory/Meditory/Features/Common/RoutineFormatter.swift
@@ -12,7 +12,7 @@ enum RoutineFormatter {
     switch cycleType {
     case 1:
       // 월=0, 화=1, 수=2, 목=3, 금=4, 토=5, 일=6
-      let map = ["월","화","수","목","금","토","일"]
+      let map = ["일", "월", "화", "수", "목", "금", "토"]
       let days = cycleValue
         .split(whereSeparator: { ", ".contains($0) })
         .compactMap { Int($0) }
