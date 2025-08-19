d9c02f9506d108859d1b9ef239bcd231f794a40a
drfranken <daranic9@gmail.com>
2025-08-19 17:35:55 +0900

    feat: ExtraInfo @Model에 @unchecked Sendable 추가 (SCRUM-166)

--- Meditory/Meditory/Persistence/ExtraInfo.swift
+++ Meditory/Meditory/Persistence/ExtraInfo.swift
@@ -10,7 +10,7 @@ enum ExtraInfoType: String, Codable, CaseIterable {
 }
 
 @Model
-final class ExtraInfo {
+final class ExtraInfo: @unchecked Sendable {
   @Attribute(.unique) var key: String
   var value: String
   private var typeRaw: String
