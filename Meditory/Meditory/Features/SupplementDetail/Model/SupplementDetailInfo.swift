26caaf1af005975cf501c9f6c8a98ca04859f047
윤혜주 <assaw102@gmail.com>
2025-08-27 22:36:06 +0900

    feat: SupplementDetailInfo Model 추가(SCRUM-225)

---
+++ Meditory/Meditory/Features/SupplementDetail/Model/SupplementDetailInfo.swift
@@ -0,0 +1,17 @@
+//
+//  Display.swift
+//  Meditory
+//
+//  Created by 윤혜주 on 8/27/25.
+//
+
+import Foundation
+
+struct SupplementDetailInfo {
+  let userTimes: [String]
+  let userCycle: String
+  let pills: [String]
+  let memo: String
+  let usage: [String]
+  let precautions: [String]
+}
