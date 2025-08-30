e4915e4df2c18d592d293e409da60d5b9a4f7183
홍승아 <howtofindjayu@gmail.com>
2025-08-30 15:24:35 +0900

    chore: Navigationbar iconSize에 defaultFontSize 적용 (SCRUM-258)

--- Meditory/Meditory/Features/Common/NavigationBar/PrimaryNavigationBar.swift
+++ Meditory/Meditory/Features/Common/NavigationBar/PrimaryNavigationBar.swift
@@ -33,7 +33,7 @@ struct PrimaryNavigationBar: View {
   private var navigationBar: some View {
     ZStack{
       let fontSize: CGFloat = isPad ? 21 : .defaultFontSize
-      let iconSize: CGFloat = isPad ? 23 : 18
+      let iconSize: CGFloat = .defaultFontSize
       
       Text(title.text)
         .font(.notoSans(size: fontSize))
