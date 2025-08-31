a009be9fe488252a355d94e77dcc3ed47c42f1ee
김현식 <dawnstory.white@gmail.com>
2025-08-31 23:52:13 +0900

    design: 개인정보동의 체크버튼 이동(SCRUM-275)

--- Meditory/Meditory/Features/Onboarding/Model/Prompt.swift
+++ Meditory/Meditory/Features/Onboarding/Model/Prompt.swift
@@ -36,7 +36,7 @@ struct Prompt {
   /// 각 단계의 화면에 프롬프트를 노출하기 위한 딕셔너리
   static let promptMessage: [Step: Prompt] = [
     .base: Prompt(title: "나만의 맞춤 영양 설정\n1분이면 끝나요"),
-    .gender: Prompt(title: "님의\n성별을 알려주세요", subtitle: "성별", info: ""),
+    .gender: Prompt(title: "님의 성별을 알려주세요", subtitle: "성별", info: ""),
     .allergy: Prompt(title: "님이\n갖고있는 식품관련 알레르기를 모두 알려주세요", info: "알레르기에 따라\n피해야하는 영양성분을 확인할 수 있어요"),
     .disease: Prompt(title: "님이\n갖고있는 질환을 모두 알려주세요", info: "해당 되는 질병을 모두 골라주세요"),
     .concern: Prompt(title: "고민되시거나 개선하고\n싶은 건강 고민을 선택해주세요", info:"개의 고민이 선택되었습니다."),
